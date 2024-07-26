5e158174fcb5702c75e9ebc02dd0a6ae9b5b2e02
kaileesegarra <158116690+kaileesegarra@users.noreply.github.com>
2024-07-26 07:17:51 -0400

    debug output

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# svg-logo-maker
-Module 10 Challenge
+# SVG Logo Maker
+
+## Description
+This is a Node.js command-line application that generates a simple logo based on user input and saves it as an SVG file.
+
+## Installation
+1. Clone the repository
+2. Run `npm install` to install dependencies
+
+## Usage
+Run `node index.js` to start the application and follow the prompts to generate a logo.
+
+## Video Demonstration
+[Link to your video demonstration](#)
--- index.js
+++ index.js
@@ -1,3 +1,58 @@
-const inquirer = require("inquirer");
-const {Circle, Square, Triangle} = require("./lib/shapes");
+// index.js
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Circle, Triangle, Square } = require('./lib/shapes');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo:',
+        validate: input => input.length <= 3 || 'Text must be three characters or less.'
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color (keyword or hexadecimal):'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape for the logo:',
+        choices: ['Circle', 'Triangle', 'Square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color (keyword or hexadecimal):'
+    }
+];
+
+function generateSVG({ text, textColor, shape, shapeColor }) {
+    let svgShape;
+    switch (shape) {
+        case 'Circle':
+            svgShape = new Circle();
+            break;
+        case 'Triangle':
+            svgShape = new Triangle();
+            break;
+        case 'Square':
+            svgShape = new Square();
+            break;
+    }
+    svgShape.setColor(shapeColor);
+
+    return `
+<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+  ${svgShape.render()}
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+</svg>`;
+}
+
+inquirer.prompt(questions).then(answers => {
+    const svgContent = generateSVG(answers);
+    fs.writeFileSync('examples/logo.svg', svgContent);
+    console.log('Generated logo.svg');
+});
